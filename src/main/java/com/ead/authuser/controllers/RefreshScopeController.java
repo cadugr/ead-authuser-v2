eb7b290d9048301f8f2bb0f117a7ceb8306d2dda
cadugr <cadu.guerra@gmail.com>
2022-06-29 15:28:00 -0300

    Alterações para adaptar o microservice para utilização de config-server

---
+++ src/main/java/com/ead/authuser/controllers/RefreshScopeController.java
@@ -0,0 +1,22 @@
+package com.ead.authuser.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//As alterações de propriedades em um config global, para funcionar, sem precisar reiniciar a aplicação, precisa estar dentro do escopo de um
+//bean do Spring.
+@RestController
+@RefreshScope
+public class RefreshScopeController {
+	
+	@Value("${authuser.refreshscope.name}")
+	private String name;
+	
+	@RequestMapping("/refreshscope")
+	public String refreshscope() {
+		return this.name;
+	}
+
+}
