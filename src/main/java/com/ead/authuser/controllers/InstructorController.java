2c4f7c72428326b6f5b2e54dfb2506995957b01e
cadugr <cadu.guerra@gmail.com>
2022-07-05 12:36:38 -0300

    Authorization Roles para Instructor

--- src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 
 //@Log4j2
@@ -29,15 +33,22 @@ public class InstructorController {
 	@Autowired
 	UserService userService;
 	
+	@Autowired
+	RoleService roleService;
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping("/subscription")
 	public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
 		Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
 		if(!userModelOptional.isPresent()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
 		} else {
+			RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+					.orElseThrow(() -> new RuntimeException("Erro: Role is Not Found."));
 			var userModel = userModelOptional.get();
 			userModel.setUserType(UserType.INSTRUCTOR);
 			userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+			userModel.getRoles().add(roleModel);
 			userService.updateUser(userModel);
 			return ResponseEntity.status(HttpStatus.OK).body(userModel);
 		}
